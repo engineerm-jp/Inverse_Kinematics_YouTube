5a89fc0925cabb8a99824d30a159aea218f491d2
Engineer M <75647798+engineerm-jp@users.noreply.github.com>
2021-07-07 21:29:55 +1200

    Update IK.py

--- IK.py
+++ IK.py
@@ -6,10 +6,10 @@
 # YouTube: https://youtu.be/YCw0JkgeTv8
 # Author: EngineerM
 
-from math import sin,cos,acos,atan,sqrt,degrees,radians, pi
+from math import sin,cos,asin,acos,atan,sqrt,degrees,radians, pi
 
 def coordinate_to_degrees(x, y): # function to convert coordinates to angles from the x-axis (0~360)
-    x += 0.00001    
+    x += 0.00001 # this is to avoid zero division error in case x == 0
  
     if x >= 0 and y >= 0:   # first quadrant
         angle = degrees(atan(y/x))
@@ -23,19 +23,13 @@ def coordinate_to_degrees(x, y): # function to convert coordinates to angles fro
 
 
 # try the following code to see how it works
-'''
-test_coordinates = [[0,0], [1,1], [0,1], [-1,1], [-1, 0], [-1,-1], [0, -1], [1,-1]]
+'''test_coordinates = [[0,0], [1,1], [0,1], [-1,1], [-1, 0], [-1,-1], [0, -1], [1,-1]]
 for XY in test_coordinates: 
-    print(f'[{XY[0]}, {XY[1]}] in degrees is {coordinate_to_degrees(XY[0], XY[1])}')
-'''
+    print(f'[{XY[0]}, {XY[1]}] in degrees is {coordinate_to_degrees(XY[0], XY[1])}')'''
 
 
-def IK(pos): 
-    # this code calculates the following angles
-    # theta1 : this is the angle of the leg from the x-axis about the z-axis
-    # theta2 : this is the angle from the x-axis about the y-axis. Note that depending on the configuration of the robot's joint, you may need to
-    #          adjust the output. For example, add offset, and/or revert the sign of the angle.
-    # theta3 : this is the angle the for the third servo motor and is measured from the axis that is co-linear to the femur. 
+
+def IK(pos): # pos = [x, y, z]
  
     x, y, z = pos[0], pos[1], pos[2]
     x += 0.0000001 # this is to avoid zero-division math error
@@ -47,22 +41,23 @@ def IK(pos):
 
     theta1 = coordinate_to_degrees(x, y)    # this is the angle from x-axis in anticlockwise rotation
 
+
     # remove the offset due to the length of coxa    
     x -= coxa*cos(radians(theta1))
     y -= coxa*sin(radians(theta1))
-
-    #print(x,y)
+    
+    if theta1 > 180: 
+        theta1 -= 360
 
     P = sqrt(x**2 + y**2)
         
-    if P > femur + tibia: 
-        P = femur + tibia 
+    if sqrt(x**2 + y**2 + z**2) > femur + tibia: 
+        print("MATH ERROR: coordinate too far")
+        return [theta1, 0, 0]
     
     alpha = atan(z/P)
 
     c = sqrt(P**2 + z**2)
-    print(c)
-
     
     beta = acos((femur**2+c**2-tibia**2)/(2*femur*c))
     theta2 = beta + alpha
@@ -70,11 +65,14 @@ def IK(pos):
     
     return round(theta1,1), round(degrees(theta2),1), round(degrees(theta3),1)
 
-# Example of usage
-'''
-angles = IK([7.07, -7.07, 10])
+'''# Example of usage
+
+angles = IK([-7.07, 7.07, 10])
 print(angles)
-'''
+
+angles = IK([7.07, 7.07, 10])
+print(angles)'''
+
 
 def IK_leg(pos, LegID=1): # modified version of IK(), and will take the offset angles of each leg into account
     x, y, z = pos[0], pos[1], pos[2]
@@ -85,12 +83,10 @@ def IK_leg(pos, LegID=1): # modified version of IK(), and will take the offset a
     femur   = 10      # femur length
     tibia   = 10      # tibia length
 
-    # offset angle of each leg from x axis (+ve counterclockwise) <- put your original values
-    offset_angles = [0, 45, 135, 180, 225, 315]
+    # offset angle of each leg from x axis (+ve counterclockwise)
+    offset_angles = [0, 45, 135, 180, 225, 315] #  <- put your original values; there is not limit on numbers of leg, but they should all be between 0 ~ 360 degrees
 
     theta1 = coordinate_to_degrees(x, y)    # angle from x-axis in anticlockwise rotation
-
-   
 
     # remove the offset due to the length of coxa    
     x -= coxa*cos(radians(theta1))
@@ -98,30 +94,38 @@ def IK_leg(pos, LegID=1): # modified version of IK(), and will take the offset a
 
      # subtract the offset angle from the x axis
     theta1 -= offset_angles[LegID-1] # theta1 = theta1 - offset_angles[LegID-1]
-
-    P = sqrt(x**2 + y**2)
-        
-    if P > femur + tibia: 
-        P = femur + tibia 
-        print("coordinate too far")
     
-    alpha = atan(z/P)
+    if theta1 > 180: # keep the theta1 between -180 <= theta1 <= 180
+        theta1 -= 360
 
-    c = sqrt(P**2 + z**2)
-
-    beta = acos((femur**2+c**2-tibia**2)/(2*femur*c))
-    theta2 = beta + alpha
-    theta3 = acos((tibia**2+femur**2-c**2)/(2*tibia*femur)) - pi
+    P = sqrt(x**2 + y**2) # calculate length P
+        
+    if sqrt(x**2 + y**2 + z**2) > femur + tibia: # detect math error
+        print("MATH ERROR: coordinate too far")
+        return [theta1, 0, 0]
+    
+    alpha = atan(z/P) # calculate angle alpha
+    c = sqrt(P**2 + z**2) # calculate length c
+    
+    beta = acos((femur**2+c**2-tibia**2)/(2*femur*c)) # calculate angle beta
+    theta2 = beta + alpha # find theta2
+    theta3 = acos((tibia**2+femur**2-c**2)/(2*tibia*femur)) - pi # find theta3
     
     return round(theta1,1), round(degrees(theta2),1), round(degrees(theta3),1)
-'''
-# Example of usage
-example_pos =  [[ 10,  10, -10],
+
+
+'''# Example of use
+example_positions =  [[ 10,  10, -10],
                 [-10,  10, -10],
                 [-10, -10, -10],
                 [ 10, -10, -10]]
-legID = 5 
-for pos in example_pos: 
-    angles = IK_leg(pos, legID)
-    print(f'theta1 = {angles[0]}, theta2 = {angles[1]}, theta3 = {angles[2]}')
-'''
+
+legID = 1 # choose which leg  
+for pos in example_positions: 
+    angles = IK_leg(pos, legID) # calculate the angles of joints 
+    print(f'Leg = {legID}, theta1 = {angles[0]}, theta2 = {angles[1]}, theta3 = {angles[2]}') # display the output
+
+
+angles = IK_leg([20,20,20], LegID=1) # when the coordinate is too far away
+print(f'Leg = {legID}, theta1 = {angles[0]}, theta2 = {angles[1]}, theta3 = {angles[2]}')'''
+ 
